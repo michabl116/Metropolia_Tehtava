020289a32ebb5c3e5f2fbf9d1b5bddcf1aaa9691
michabl116 <michabl@metropolia.fi>
2024-08-22 13:15:53 +0300

    Tehtava moduuli1

---
+++ Moduuli2/Tehtava5.py
@@ -0,0 +1,8 @@
+leiviska = float(input("Anna leiviskä:"))
+naula =float(input("Anna naula:"))
+luoti = float(input("Anna luoti:"))
+LUOTI= 13,3
+NAULA= 32 * LUOTI
+LEIVISKA = 20* NAULA
+
+Kilograma=
---
+++ Moduuli2/tehtava4.py
@@ -0,0 +1,7 @@
+luku1=int(input("Anna luku1:"))
+luku2=int(input("Anna luku2:"))
+luku3=int(input("Anna luku3:"))
+suma = luku1+luku2+luku3
+keskiarvo= (luku1+luku2+luku3)/3
+print(F"suma: {suma}")
+print(F"keskiarvo: {keskiarvo}")
