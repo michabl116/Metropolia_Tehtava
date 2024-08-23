c4cd06959d4fc11000e864673244d7896b43b97e
michabl116 <michabl@metropolia.fi>
2024-08-24 00:23:50 +0300

    Tehtava moduuli1 ja 2

--- Moduuli2/Tehtava5.py
+++ Moduuli2/Tehtava5.py
@@ -1,8 +1,14 @@
-leiviska = float(input("Anna leiviskä:"))
-naula =float(input("Anna naula:"))
-luoti = float(input("Anna luoti:"))
-LUOTI= 13,3
-NAULA= 32 * LUOTI
-LEIVISKA = 20* NAULA
+import math
 
-Kilograma=
+leiviska_luku = float(input("Anna leiviskä:"))
+naula_luku =float(input("Anna naula:"))
+luoti_luku= float(input("Anna luoti:"))
+Luoti= 13.3
+Naula= 32 * Luoti
+Leiviska=20* Naula
+luku1 = leiviska_luku*Leiviska
+luku2 = naula_luku*Naula
+luku3 = luoti_luku*Luoti
+kokosumma= luku1+luku2+luku3
+sum= str(kokosumma)
+print((sum[0:2])+" Kilogramma ja "+(sum[2:])+ " grammaa")
