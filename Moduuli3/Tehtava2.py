0cd556b77ff05cce20c471bcfe662c0a33ecda6e
michabl116 <michabl@metropolia.fi>
2024-08-27 09:35:54 +0300

    Tehtava moduuli 3

---
+++ Moduuli3/Tehtava2.py
@@ -0,0 +1,18 @@
+
+
+hytttiluoka = input("Anna hyttiluoka (LUX, A, B, C):")
+if hytttiluoka == "LUX":
+    print("LUX on parvekkeellinen hytti yläkannella.")
+elif hytttiluoka == "A":
+    print("A on ikkunallinen hytti autokannen yläpuolella.")
+elif hytttiluoka == "B":
+    print("B on ikkunaton hytti autokannen yläpuolella.")
+elif hytttiluoka == "C":
+    print("C on ikkunaton hytti autokannen alapuolella.")
+else:
+    print("Virheellinen hyttiluokka!")
+
+
+
+
+
