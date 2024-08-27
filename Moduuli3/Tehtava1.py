21b75cae213a459fc15d7874c6626e86598d1d97
michabl116 <michabl@metropolia.fi>
2024-08-27 07:06:49 +0300

    Tehtava moduuli1 ja 2

---
+++ Moduuli3/Tehtava1.py
@@ -0,0 +1,5 @@
+
+Kala_koko= int(input("Kuinka senttimetria kala on: "))
+if Kala_koko < 37 :
+    kala=37-Kala_koko
+    print(F"Kala ei ole tarpeeksi iso, laita se takaisin järveen,puuttuu {kala} cm")
