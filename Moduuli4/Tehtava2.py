36632a945d541400d3a2cd9cc9b25ecdd69ea801
michabl116 <michabl@metropolia.fi>
2024-08-28 11:01:27 +0300

    Tehtava moduuli 4

---
+++ Moduuli4/Tehtava2.py
@@ -0,0 +1,9 @@
+
+luku1 = float(input("Anna lukuu tuumissa:"))
+while luku1 > 1 :
+    print(f" Luku sentrimetria on: {luku1 * 2.54} cm")
+    luku1 = float(input("Anna lukuu tuumissa:"))
+else:
+    print("Lopu ohjelma")
+
+
---
+++ Moduuli4/tehtava1.py
@@ -0,0 +1,13 @@
+
+
+lukunumero1=1
+while lukunumero1 >0 and lukunumero1 < 1000:
+    luku1= lukunumero1//3
+    while luku1= :
+        print(luku1)
+
+    lukunumero1 = lukunumero1 + 1
+
+
+
+
