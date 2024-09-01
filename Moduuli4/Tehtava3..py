dc8a8c1e38246454a9ad0f4ff3127b3eb47df24b
michabl116 <michabl@metropolia.fi>
2024-09-01 09:12:17 +0300

    tehtava3  puutu osat

--- Moduuli4/Tehtava3..py
+++ Moduuli4/Tehtava3..py
@@ -1,17 +1,17 @@
 
-luku = input("Anna luku:")
+luku = int(input("Anna luku:"))
 while True:
-    print(luku)
-    luku = input("Anna luku: ")
-    if luku == " ":
+    print(f"{luku}")
+    luku = int(input("Anna luku: "))
+    num = luku
+    if luku == "":
 
-        num = int(luku)
         if num < min:
             min = luku
-        if luku > max:
-            max = luku
-            print(f"Pienin luku: {min}")
-            print(f"Suurin luku: {max}")
+            if luku > max:
+                max = luku
+                print(f"Pienin luku: {min}")
+                print(f"Suurin luku: {max}")
 
 
 
