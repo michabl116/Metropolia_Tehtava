ff1175f5638df752c3adcba131f8ff6b820345ae
micha <michabl@metropolia.fi>
2024-10-17 23:54:46 +0300

    Moduuli9

---
+++ moduuli10/Tehtava1.py
@@ -0,0 +1,11 @@
+class Hissi ():
+    def __init__(self):
+        alimman =0
+        ylimmäan =0
+    def siiry_kerrokseen(self):
+        pass
+    def kerros_ylös(self):
+
+        pass
+    def kerros_alas(self):
+        pass
