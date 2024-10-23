f35931323990c0fb88b747643913bdc5c267c824
michabl116 <michabl@metropolia.fi>
2024-10-24 01:14:29 +0300

    tehtävä4

--- Moduuli9/Tehtava4.py
+++ Moduuli9/Tehtava4.py
@@ -3,6 +3,12 @@ class autokilpailu:
         self.rekisteri=rekisteri
         self.kuljettu_matka= 0
 
+    def auto(self, auto_rec):
+        for auto_rec in range:
+            print(f"ABC-{auto_rec}")
 
-lista_autokilpailu = ["ABC1","ABC2","ABC3","ABC4","ABC5","ABC6",
-                      "ABC7","ABC7""ABC8""ABC9","ABC10"]
+Auto = autokilpailu()
+
+
+#lista_autokilpailu = ["ABC1","ABC2","ABC3","ABC4","ABC5","ABC6",
+                      #"ABC7","ABC7""ABC8""ABC9","ABC10"]
