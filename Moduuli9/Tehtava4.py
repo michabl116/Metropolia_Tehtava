5277acf363d037f165b29ef545dd0ea533269964
micha <michabl@metropolia.fi>
2024-10-23 17:33:12 +0300

    Moduuli9

---
+++ Moduuli9/Tehtava4.py
@@ -0,0 +1,8 @@
+class autokilpailu:
+    def __init__(self,rekisteri):
+        self.rekisteri=rekisteri
+        self.kuljettu_matka= 0
+
+
+lista_autokilpailu = ["ABC1","ABC2","ABC3","ABC4","ABC5","ABC6",
+                      "ABC7","ABC7""ABC8""ABC9","ABC10"]
