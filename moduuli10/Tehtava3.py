a6f8aa0d675f9ee1ad70cdd196ac102898f2fb12
micha <michabl@metropolia.fi>
2024-10-28 16:56:27 +0200

    Moduuli10

--- moduuli10/Tehtava3.py
+++ moduuli10/Tehtava3.py
@@ -4,7 +4,7 @@ class Hissi:
         self.alimman = alimman
         self.ylimman = ylimman
 
-        print(f"Hissi on kerro {Hissi.nykyinen_kerros}.")
+        #print(f"Hissi on kerro {Hissi.nykyinen_kerros}.")
 
     def siirry_kerrokseen(self, kerros):
         while Hissi.nykyinen_kerros != kerros:
@@ -26,12 +26,15 @@ class Hissi:
 
 
 class Talo:
+    halytus=False
     def __init__(self, ali,yli,luku_mara):
 
         self.alima = ali
         self.ylima= yli
         self.luku_mara = luku_mara
         self.hissit = []
+
+
         for i in range(luku_mara):
             self.hissit.append(Hissi(self.alima,self.ylima))
 
@@ -41,8 +44,18 @@ class Talo:
         if kerro_numero >=1 and kerro_numero<= self.luku_mara:
             self.hissit[kerro_numero-1].siirry_kerrokseen(hissit_kerro)
 
+    def palohalytys(self):
+        print()
+        print()
+        for hissi in self.hissit:
+            hissi.siirry_kerrokseen(self.alima)
+
+
+
 
 
 
 talo = Talo(1,10,4)
 talo.aja_hissia(3,5)
+talo.palohalytys()
+
